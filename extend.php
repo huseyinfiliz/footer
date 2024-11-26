d6e2d0baf7d34b765aa78d36844d1f909534648e
Hüseyin Filiz <37220000+huseyinfiliz@users.noreply.github.com>
2024-11-26 14:55:36 +0300

    Create extend.php

---
+++ extend.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace huseyinfiliz\footer;
+
+use Flarum\Api\Serializer\ForumSerializer;
+use Flarum\Extend;
+
+return [
+    (new Extend\Frontend('forum'))
+        ->js(__DIR__.'/js/dist/forum.js')
+        ->css(__DIR__.'/resources/less/forum.less'),
+
+    (new Extend\Frontend('admin'))
+        ->js(__DIR__.'/js/dist/admin.js'),
+
+    new Extend\Locales(__DIR__.'/resources/locale'),
+
+    (new Extend\ApiSerializer(ForumSerializer::class))
+        ->attributes(Listeners\LoadSettingsFromDatabase::class),
+];
